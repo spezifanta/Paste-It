ec3209dc53f1366c77ed42269b86094f79661668
spezi|Fanta <fanta@spezi-clan.de>
2012-01-20 19:09:38 +0100

    Added Quantico font, Mimetype and download size

--- paste/views.py
+++ paste/views.py
@@ -25,30 +25,44 @@ def add(request):
 def view(request, pk, output_type=None):
     paste = get_object_or_404(Paste, pk=pk)
 
-    # TODO: Add update
     # Update view counter
-    #paste.view += 1
-    #paste.save(update=True)
+    paste.views += 1
+    paste.save()
 
     if output_type:
         response = HttpResponse(paste.content)
         response['Content-Type'] = 'text/plain; charset=utf-8' # Fix UTF-8
+        response['Conent-Length'] = len(paste.content)
 
         if output_type == 'raw':
             return response
+
         elif output_type == 'download':
-            # TODO: add download size and mimetype
             response['Content-Disposition'] = 'attachment; filename=%s.%s' % (paste.id, paste.language.ext)
+
+            if len(paste.language.mimetype) > 0:
+                response['Content-Type'] = '%s; charset=utf-8' % paste.language.mimetype 
+
             return response
         # Invalid output types
         else:
             return redirect('/%s' % paste.pk)
     else:
+        # Force line brakes
+        new_content = []
+        for line in paste.content.split('\n'):
+            while len(line) > 100:
+                pos = line.rfind(' ', 0, 100)
+                new_content.append('%s\n' % line[0:pos].strip())
+                line = line[pos:]
+
+        new_content = ''.join(new_content)
+
         lexer = lexers.get_lexer_by_name(paste.language.short)
         new_content = map(lambda line: highlight(line,
                                                  lexer,
                                                  HtmlFormatter(nowrap=True)),
-                          paste.content.split('\n'))
+                          new_content.split('\n'))
 
         form = PasteForm(instance=paste)
         tpl_var = {
